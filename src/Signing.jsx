80169cf983db32cce4f1e008b428c90766d054b6
Hemakumar12345 <ghemakumar4321@gmail.com>
2025-05-16 17:14:05 +0530

    Update Signing.jsx

--- src/Signing.jsx
+++ src/Signing.jsx
@@ -1,59 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { LoginUser } from './store';
 import './Signing.css';
 
+
 function Signing() {
-  const [showSignup, setShowSignup] = useState(false);
-  const { register, handleSubmit } = useForm();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  let {register,handleSubmit}=useForm();
+  let dispatch=useDispatch();
+  let navigate=useNavigate();
 
-  const handleLogin = (data) => {
+  let myFunc=(data)=>{
     dispatch(LoginUser(data));
-    navigate('/veg');
-  };
-
-  const handleSignup = (data) => {
-    // Handle signup logic (e.g., dispatch signup action)
-    console.log('User registered:', data);
-    setShowSignup(false); // After signup, go back to login
-  };
+    navigate("/veg");
+  }
 
   return (
     <div className="signin-container">
-      {showSignup ? (
-        <>
-          <h2>User SignUp</h2>
-          <form onSubmit={handleSubmit(handleSignup)}>
-            <input type="text" placeholder="Username" {...register('username')} />
-            <input type="email" placeholder="Email" {...register('email')} />
-            <input type="password" placeholder="Password" {...register('password')} />
-            <button type="submit">SignUp</button>
-          </form>
-          <p>
-            Already have an account?{' '}
-            <button onClick={() => setShowSignup(false)} className="link-button">SignIn</button>
-          </p>
-        </>
-      ) : (
-        <>
-          <h2>User SignIn</h2>
-          <form onSubmit={handleSubmit(handleLogin)}>
-            <input type="text" placeholder="Username" {...register('username')} />
-            <input type="password" placeholder="Password" {...register('password')} />
-            <button type="submit">SignIn</button>
-          </form>
-          <p>
-            New user?{' '}
-            <button onClick={() => setShowSignup(true)} className="link-button">SignUp</button>
-          </p>
-        </>
-      )}
+<h2>User SignIn</h2>
+<form onSubmit={handleSubmit(myFunc)}>
+  <input type="text" placeholder="Username" {...register('username')}/>
+  <input type="password" placeholder="Password" {...register('password')}/>
+  <button type="submit">SignIn</button>
+</form>
+<p>
+ New user?<a href="/signout">SignUp</a>
+</p>
+
     </div>
-  );
+  )
 }
 
-export default Signing;
+export default Signing 
