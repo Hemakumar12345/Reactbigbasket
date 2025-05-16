3e2d6d18b5b2f87e6de35d2002c6761151c9457d
Hemakumar12345 <ghemakumar4321@gmail.com>
2025-05-16 17:07:38 +0530

    Update Signing.jsx

--- src/Signing.jsx
+++ src/Signing.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { LoginUser } from './store';
 import './Signing.css';
 
-
 function Signing() {
-  let {register,handleSubmit}=useForm();
-  let dispatch=useDispatch();
-  let navigate=useNavigate();
+  let { register, handleSubmit } = useForm();
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
 
-  let myFunc=(data)=>{
+  let myFunc = (data) => {
     dispatch(LoginUser(data));
     navigate("/veg");
-  }
+  };
 
   return (
     <div className="signin-container">
-<h2>User SignIn</h2>
-<form onSubmit={handleSubmit(myFunc)}>
-  <input type="text" placeholder="Username" {...register('username')}/>
-  <input type="password" placeholder="Password" {...register('password')}/>
-  <button type="submit">SignIn</button>
-</form>
-<p>
- New user?<a href="/signout">SignUp</a>
-</p>
-
+      <h2>User SignIn</h2>
+      <form onSubmit={handleSubmit(myFunc)}>
+        <input type="text" placeholder="Username" {...register('username')} />
+        <input type="password" placeholder="Password" {...register('password')} />
+        <button type="submit">SignIn</button>
+      </form>
+      <p>
+        New user? <Link to="/signup">SignUp</Link>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Signing
+export default Signing;
